b231bdd9503d02c1120a63aecb6fcf66467a9b83
kperumal-bts <kperumal@biotechsquare.com>
2020-11-15 22:45:11 -0500

    remove irrelevant scripts

--- pretty_print_json.py
+++
@@ -1,8 +0,0 @@
-import sys
-import json
-
-if __name__ == '__main__':
-    with open(sys.argv[1], 'r') as file_reader, \
-            open(sys.argv[2], 'w') as file_writer:
-        data = json.load(file_reader)
-        json.dump(data, file_writer, indent=4, sort_keys=True)
