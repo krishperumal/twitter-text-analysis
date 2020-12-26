7949646739be9c14a6d93514c9c5b7683df1409b
kperumal-bts <kperumal@biotechsquare.com>
2020-12-26 17:09:27 -0500

    fix script to create output dir if not present and output csv rows as lists instead of dicts

--- get_ngram_frequencies.py
+++ get_ngram_frequencies.py
@@ -1,4 +1,5 @@
-"""Get n-gram (uni, bi and trigram) frequencies for all tweets in input directory or file.
+"""Get n-gram (uni, bi and trigram) frequencies for all tweets in JSON format
+in input directory or file.
 """
 import json
 import os
@@ -6,16 +7,21 @@ import spacy
 import csv
 from argparse import ArgumentParser
 
+
 def parse_arguments():
     parser = ArgumentParser('Get n-gram (uni, bi and trigram) frequencies '
                             'for all tweets in input directory or file.')
     parser.add_argument('--input_path', '-i', type=str,
                         help='Path to input directory or file containing '
-                        'tweets in JSON format.'
+                        'tweets in JSON format.')
     parser.add_argument('--output_dir_path', '-o', type=str,
                         help='Path to output directory containing CSV files '
                         'with n-gram frequencies (one each for uni, bi and '
                         'trigrams)')
+    parser.add_argument('--spacy_model', '-s', type=str,
+                        default='en_core_web_sm',
+                        help='Name of spacy model to use.'
+                        )
     return parser.parse_args()
 
 
@@ -27,17 +33,21 @@ if __name__ == '__main__':
     output_path = args.output_dir_path
 
     # load spacy nlp module
-    nlp = spacy.load('en_core_web_sm')
+    nlp = spacy.load(args.spacy_model)
 
     # check if input path is a directory or list
     input_file_paths = []
     # if input path is a directory, make a list of all files inside it
     if os.path.isdir(input_path):
-        input_file_paths = [os.path.join(input_path, f) 
-                            for f in os.listdir(input_path)]
+        input_file_paths = [os.path.join(input_path, f)
+                            for f in os.listdir(input_path)
+                            if f.endswith('.json')]
     # if input path is not a directory, just get the single input file path
     else:
-        input_file_paths = [input_path]
+        if input_path.endswith('.json'):
+            input_file_paths = [input_path]
+        else:
+            input_file_paths = []
 
     # initialize ngram frequency dictionaries
     unigram_freq = {}
@@ -64,10 +74,10 @@ if __name__ == '__main__':
             word_list = [w for w in doc]
 
             for i in range(len(word_list)):
-                # check if current token is a stopword, punctuation or space 
+                # check if current token is a stopword, punctuation or space
                 # character
                 if word_list[i].is_stop or word_list[i].is_punct or \
-                    word_list[i].is_space:
+                        word_list[i].is_space:
                     continue
 
                 # get unigram text
@@ -90,7 +100,7 @@ if __name__ == '__main__':
                     else:
                         bigram_freq[bigram_text] = bigram_freq[bigram_text]+1
 
-                    # check if next two words exist (for calculating trigram 
+                    # check if next two words exist (for calculating trigram
                     # frequency)
                     if i < len(word_list) - 2:
                         # get trigram text
@@ -103,35 +113,35 @@ if __name__ == '__main__':
                             trigram_freq[trigram_text] = \
                                 trigram_freq[trigram_text]+1
 
-    unigram_freq_list = sorted(unigram_freq.items(), key=lambda k: k[1], 
+    unigram_freq_list = sorted(unigram_freq.items(), key=lambda k: k[1],
                                reverse=True)
-    bigram_freq_list = sorted(bigram_freq.items(), key=lambda k: k[1], 
+    bigram_freq_list = sorted(bigram_freq.items(), key=lambda k: k[1],
                               reverse=True)
-    trigram_freq_list = sorted(trigram_freq.items(), key=lambda k: k[1], 
+    trigram_freq_list = sorted(trigram_freq.items(), key=lambda k: k[1],
                                reverse=True)
 
-    # write frequencies to output_path with name 
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    # write frequencies to output_path with name
     # "<input_name>_<ngram_n>-gram.txt"
     # unigram
     output_file_name = 'unigram.csv'
     output_file_path = os.path.join(output_path, output_file_name)
     with open(output_file_path, 'w') as file_writer:
-        csv_writer = csv.DictWriter(file_writer, 
-                                    fieldnames=['unigram', 'frequency'])
+        csv_writer = csv.writer(file_writer)
         csv_writer.writerows(unigram_freq_list)
 
     # bigram
     output_file_name = 'bigram.csv'
     output_file_path = os.path.join(output_path, output_file_name)
     with open(output_file_path, 'w') as file_writer:
-        csv_writer = csv.DictWriter(file_writer, 
-                                    fieldnames=['bigram', 'frequency'])
+        csv_writer = csv.writer(file_writer)
         csv_writer.writerows(bigram_freq_list)
 
     # trigram
     output_file_name = 'trigram.csv'
     output_file_path = os.path.join(output_path, output_file_name)
     with open(output_file_path, 'w') as file_writer:
-        csv_writer = csv.DictWriter(file_writer, 
-                                    fieldnames=['trigram', 'frequency'])
+        csv_writer = csv.writer(file_writer)
         csv_writer.writerows(trigram_freq_list)
